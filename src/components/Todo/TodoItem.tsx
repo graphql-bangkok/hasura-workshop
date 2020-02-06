fbb30cbbb203485376110d9c8e15ea08000a510f
Lucas Munhoz <ln.munhoz@gmail.com>
2020-02-06 11:07:30 +0700

    Toggle todo

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,17 +1,36 @@
-import * as React from 'react';
+import * as React from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 
 export type TodoItem = {
-  id: number,
-  title: string,
-  is_completed: boolean
+  id: number;
+  title: string;
+  is_completed: boolean;
 };
 
 interface TodoItemType {
-  index: number,
-  todo: TodoItem
-};
+  index: number;
+  todo: TodoItem;
+}
 
-const TodoItem = ({index, todo}: TodoItemType) => {
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
+    update_todos(
+      where: { id: { _eq: $id } }
+      _set: { is_completed: $isCompleted }
+    ) {
+      affected_rows
+      returning {
+        id
+        title
+        is_completed
+      }
+    }
+  }
+`;
+
+const TodoItem = ({ index, todo }: TodoItemType) => {
+  const [todoUpdate] = useMutation(TOGGLE_TODO);
 
   const removeTodo = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -19,6 +38,24 @@ const TodoItem = ({index, todo}: TodoItemType) => {
   };
 
   const toggleTodo = () => {
+    todoUpdate({
+      variables: { id: todo.id, isCompleted: !todo.is_completed },
+      optimisticResponse: {
+        __typename: "mutation_root",
+        update_todos: {
+          __typename: "todos_mutation_response",
+          affected_rows: 1,
+          returning: [
+            {
+              __typename: "todos",
+              id: todo.id,
+              title: todo.title,
+              is_completed: !todo.is_completed
+            }
+          ]
+        }
+      }
+    });
   };
 
   return (
@@ -31,17 +68,15 @@ const TodoItem = ({index, todo}: TodoItemType) => {
             id={todo.id!.toString()}
             onChange={() => toggleTodo()}
           />
-          <label htmlFor={todo.id!.toString()}/>
+          <label htmlFor={todo.id!.toString()} />
         </div>
       </div>
 
-      <div className={"labelContent" + (todo.is_completed ? " completed" : '')}>
-        <div>
-          {todo.title}
-        </div>
+      <div className={"labelContent" + (todo.is_completed ? " completed" : "")}>
+        <div>{todo.title}</div>
       </div>
 
-      <button className="closeBtn" onClick={(e) => removeTodo(e)}>
+      <button className="closeBtn" onClick={e => removeTodo(e)}>
         x
       </button>
     </li>
