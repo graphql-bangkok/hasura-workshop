5c14a98cd6582b25ddba3aca258e4313c2101135
Lucas Munhoz <ln.munhoz@gmail.com>
2020-02-06 10:43:53 +0700

    Setup Apollo and first query

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,58 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Header from './Header';
-import TodoPrivateWrapper from './Todo/TodoPrivateWrapper';
-import TodoPublicWrapper from './Todo/TodoPublicWrapper';
-import OnlineUsersWrapper from './OnlineUsers/OnlineUsersWrapper';
+import Header from "./Header";
+import TodoPrivateWrapper from "./Todo/TodoPrivateWrapper";
+import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
+import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
 import { useAuth0 } from "./Auth/react-auth0-spa";
 
-const App = ({idToken}:{idToken:string}) => {
-  const { loading, logout  } = useAuth0();
-  if(loading) {
-    return (<div>Loading...</div>);
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const createApolloClient = (authToken: string) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: "https://learn.hasura.io/graphql",
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    }),
+    cache: new InMemoryCache()
+  });
+};
+
+const App = ({ idToken }: { idToken: string }) => {
+  const { loading, logout } = useAuth0();
+  const client = createApolloClient(idToken);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
   return (
-    <div>
-      <Header logoutHandler={logout} />
-      <div className="container-fluid p-left-right-0">
-        <div className="col-xs-12 col-md-9 p-left-right-0">
-          <div className="col-xs-12 col-md-6 sliderMenu p-30">
-            <TodoPrivateWrapper />
+    <ApolloProvider client={client}>
+      <div>
+        <Header logoutHandler={logout} />
+        <div className="container-fluid p-left-right-0">
+          <div className="col-xs-12 col-md-9 p-left-right-0">
+            <div className="col-xs-12 col-md-6 sliderMenu p-30">
+              <TodoPrivateWrapper />
+            </div>
+            <div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
+              <TodoPublicWrapper />
+            </div>
           </div>
-          <div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
-            <TodoPublicWrapper />
-          </div>
-        </div>
-        <div className="col-xs-12 col-md-3 p-left-right-0">
-          <div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
-            <OnlineUsersWrapper />
+          <div className="col-xs-12 col-md-3 p-left-right-0">
+            <div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
+              <OnlineUsersWrapper />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
--- src/components/Todo/TodoPrivateList.tsx
+++ src/components/Todo/TodoPrivateList.tsx
@@ -1,58 +1,61 @@
 import React, { Fragment, useState } from "react";
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
 type Todo = {
-  id: number,
-  title: string,
-  is_completed: boolean
+  id: number;
+  title: string;
+  is_completed: boolean;
 };
 
-const TodoPrivateList = () => {
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(
+      where: { is_public: { _eq: false } }
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      is_completed
+    }
+  }
+`;
 
+const TodoPrivateList = () => {
+  const { data, loading } = useQuery(GET_MY_TODOS);
   const [filter, setFilter] = useState<string>("all");
 
-  const todos = [
-    {
-      id: 1,
-      title: "This is private todo 1",
-      is_completed: true
-    },
-    {
-      id: 2,
-      title: "This is private todo 2",
-      is_completed: false
-    }
-  ];
+  if (loading) {
+    return <div> Loading...</div>;
+  }
 
   const filterResults = (filter: string): void => {
     setFilter(filter);
   };
 
-  const clearCompleted = () => {
-  };
+  const clearCompleted = () => {};
 
-  let filteredTodos = todos;
+  let filteredTodos = data.todos;
   if (filter === "active") {
-    filteredTodos = todos.filter((todo: Todo) => todo.is_completed !== true);
+    filteredTodos = data.todos.filter(
+      (todo: Todo) => todo.is_completed !== true
+    );
   } else if (filter === "completed") {
-    filteredTodos = todos.filter((todo: Todo) => todo.is_completed === true);
+    filteredTodos = data.todos.filter(
+      (todo: Todo) => todo.is_completed === true
+    );
   }
 
   const todoList = filteredTodos.map((todo: Todo, index: number) => (
-    <TodoItem
-      key={'item'+index}
-      index={index}
-      todo={todo}
-    />
+    <TodoItem key={"item" + index} index={index} todo={todo} />
   ));
 
   return (
     <Fragment>
       <div className="todoListWrapper">
-        <ul>
-          { todoList }
-        </ul>
+        <ul>{todoList}</ul>
       </div>
 
       <TodoFilters
@@ -63,6 +66,6 @@ const TodoPrivateList = () => {
       />
     </Fragment>
   );
-}
+};
 
 export default TodoPrivateList;
